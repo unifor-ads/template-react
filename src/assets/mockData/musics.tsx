2f1bcb7fe8792e662153520fc1c778d2bba9bd3d
Italo Fontenele <italo.fontenele.silva@gmail.com>
2023-08-30 21:10:47 -0300

    dados de teste

---
+++ src/assets/mockData/musics.tsx
@@ -0,0 +1,87 @@
+export const musics = [
+	{
+		name: 'Anjo Querubim',
+		artist: 'Limão com Mel',
+		imgSrc: 'images/musics/spencer-moore-1.jpg',
+	},
+	{
+		name: 'Me Usa',
+		artist: 'Banda Magníficos',
+		imgSrc: 'images/musics/spencer-moore-3.jpg',
+	},
+	{
+		name: 'A Praia',
+		artist: 'Mastruz com Leite',
+		imgSrc: 'images/musics/sean-o-riordan.jpg',
+	},
+	{
+		name: 'Tontos e Loucos',
+		artist: 'Aviões do Forró',
+		imgSrc: 'images/musics/carla-gomez-1.jpg',
+	},
+	{
+		name: 'Saga de um Vaqueiro',
+		artist: 'Catuaba com Amendoim',
+		imgSrc: 'images/musics/corwin-prescott-3.jpg',
+	},
+	{
+		name: 'Timidez',
+		artist: 'Forró Cavalo de Pau',
+		imgSrc: 'images/musics/card-image.png',
+	},
+	{
+		name: 'Nosso Amor',
+		artist: 'Lagosta Bronzeada',
+		imgSrc: 'images/musics/card-image.png',
+	},
+	{
+		name: 'Me Leva',
+		artist: 'Alcymar Monteiro',
+		imgSrc: 'images/musics/card-image.png',
+	},
+	{
+		name: 'Xote das Meninas',
+		artist: 'Luiz Gonzaga',
+		imgSrc: 'images/musics/card-image.png',
+	},
+	{
+		name: 'Chame Gente',
+		artist: 'Dominguinhos',
+		imgSrc: 'images/musics/card-image.png',
+	},
+	{
+		name: 'Asa Branca',
+		artist: 'Luiz Gonzaga',
+		imgSrc: 'images/musics/card-image.png',
+	},
+	{
+		name: 'Xote Nazaré',
+		artist: 'Dominguinhos',
+		imgSrc: 'images/musics/card-image.png',
+	},
+	{
+		name: 'No Rancho Fundo',
+		artist: 'Luiz Gonzaga',
+		imgSrc: 'images/musics/card-image.png',
+	},
+	{
+		name: 'Beijinho Doce',
+		artist: 'Elba Ramalho',
+		imgSrc: 'images/musics/card-image.png',
+	},
+	{
+		name: 'Frevo Mulher',
+		artist: 'Chico Buarque',
+		imgSrc: 'images/musics/card-image.png',
+	},
+	{
+		name: 'De Volta pro Aconchego',
+		artist: 'Alceu Valença',
+		imgSrc: 'images/musics/card-image.png',
+	},
+	{
+		name: 'O Xote das Meninas',
+		artist: 'Luiz Gonzaga',
+		imgSrc: 'images/musics/card-image.png',
+	},
+];

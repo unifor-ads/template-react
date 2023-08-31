b11835d6fb03f20408a6384c55b763adba16f872
Italo Fontenele <italo.fontenele.silva@gmail.com>
2023-08-31 13:23:29 -0300

    adição de responsividade dos cards de musica

--- src/assets/components/MusicCards/MusicImg.tsx
+++ src/assets/components/MusicCards/MusicImg.tsx
@@ -1,19 +1,14 @@
-import { MusicButton } from './MusicButton';
-import './music-card.css'
+import './music-card.css';
 
 interface MusicImgProps {
 	src: string;
 	alt?: string;
 }
 
-export const MusicImg = ({src, alt}: MusicImgProps) => {
+export const MusicImg = ({ src, alt }: MusicImgProps) => {
 	return (
 		<picture className='music-card-img'>
-			<img
-				src= { src }
-				alt= { alt || "" }
-			></img>
-			<MusicButton />
+			<img src={src} alt={alt || ''}></img>
 		</picture>
 	);
-}
+};

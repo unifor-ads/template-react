b8a77c383e41425541ac934a46e184745b3c532d
Italo Fontenele <italo.fontenele.silva@gmail.com>
2023-09-01 20:33:17 -0300

    adição de thema

--- src/assets/components/Navbar/index.tsx
+++ src/assets/components/Navbar/index.tsx
@@ -1,8 +1,25 @@
+import React, { useState } from 'react';
+
 import './navbar.css';
 
 import { Logo } from './Logo';
 
 export const Navbar = () => {
+	const [theme, setTheme] = useState('dark');
+
+	const toggleTheme = () => {
+		const rootElement = document.getElementById('root');
+		if (!rootElement) return;
+
+		if (theme === 'dark') {
+			rootElement.setAttribute('data-bs-theme', 'light');
+			setTheme('light');
+		} else {
+			rootElement.setAttribute('data-bs-theme', 'dark');
+			setTheme('dark');
+		}
+	};
+
 	return (
 		<nav className='navbar component navbar-expand-lg bg-body-tertiary'>
 			<div className='container-fluid'>
@@ -30,8 +47,12 @@ export const Navbar = () => {
 						</li>
 					</ul>
 
+					<a className='theme-mode dark-mode mx-3' onClick={toggleTheme}>
+						<i className='bi bi-moon-stars-fill'></i>
+					</a>
+
 					<form className='d-flex' role='login'>
-						<a className='btn nav-button' type='submit' href='signin'>
+						<a className='btn nav-button mx-2' type='submit' href='signin'>
 							Entrar
 						</a>
 					</form>
